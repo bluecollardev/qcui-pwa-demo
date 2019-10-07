e66ac0d739d04593237357cf94201ae27cfc5c18
Lucas <tech@firebrandwebsolutions.com>
2019-10-06 17:24:32 -0700

    Update serviceWorker copy path.

--- src/static/serviceWorker.js
+++ src/static/serviceWorker.js
@@ -105,7 +105,7 @@ export function register(config) {
     }
 
     window.addEventListener('load', () => {
-      const swUrl = '/src/js/service-worker.js';
+      const swUrl = '/serviceWorker.js';
 
       if (isLocalhost) {
         // This is running on localhost. Let's check if a service worker still exists or not.
